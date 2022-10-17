da42883cd47d08b2f298abbd6c49ce46173fb8be
wangk <1494649679@qq.com>
2022-10-17 17:06:13 +0800

    初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '王坤自己的页面',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
